a03501ddb7395ea7b51037daba9878e29a516698
Isabel Laguardia <isabel.laguardia@gmail.com>
2021-03-15 19:31:30 +0100

    Creación de usuarios con Laravel y TDD

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layout')
+
+@section('title', "Usuario {$user->id} - ")
+
+@section('content')
+	<h1 class="mt-5">{{ $title }} #{{ $user->id }}</h1>
+	<p class="lead">		
+		@if (!empty($user->id))
+                {{ $user->name }} - {{ $user->email }} - {{ $user->profession_id }}
+		@else
+			Este usuario no está registrado.
+		@endif
+	</p>
+        
+        <a href ="{{ url('/usuarios/') }}">Ver listado de usuarios</a>
+        <a href ="{{ url()->previous() /* para volver a la página anterior vengas de donde vengas */ }}">Ver listado de usuarios</a>
+        <a href ="{{ route('users') }}">Ver listado de usuarios</a>
+        
+        <?php  /* 
+        <a href ="{{ 
+            action([UserController::class, 'index']) /* llamando directamente al helper del controlador * / 
+                    }}">Ver listado de usuarios</a>
+            */
+        ?>
+@endsection
