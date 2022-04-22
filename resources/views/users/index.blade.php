de641783c1f6d06dceda9616160e26e8d8a35fe1
ilaguardia <isabel.laguardia@gmail.com>
2022-04-22 17:51:37 +0200

    Añadido switch comentado en blade

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,31 +3,40 @@
 @section('title', "Listado de Usuarios - ")
 
 @section('content')
-    <h1 class="mt-5">Usuarios</h1>
-    <p class="lead">
-        <a href ="{{ route('users.create') }}">Crear usuario</a>
-    </p>
-    <p class="lead">
-        @if ($users->count() == 0 )
-            No hay usuarios registrados
-        @else
-        <ul>
-            @foreach ($users as $user)
+<h1 class="mt-5">Usuarios</h1>
+<p class="lead">
+    <a href ="{{ route('users.create') }}">Crear usuario</a>
+</p>
+<p class="lead">
+    @if ($users->count() == 0 )
+    No hay usuarios registrados
+    @else
+<ul>
+    @foreach ($users as $user)
 
-            <?php /*
-             * <li><a href="{{ action([UserController::class, 'show'], ['id' => $user->id]) }}">{{ $user->name }}</a>, ({{ $user->email }})</li>
-              Este no he conseguido que funcione
-             *                          */
-            ?>
+    <?php /*
+    @switch($field)
+    @case ('dni')
+    {{ Str::limit($user->$field, 14) }}
+    @break
+    @default
+    {{ $user->$field }}
+    @endswitch
 
-            <li><a href="{{ url('/usuarios/'.$user->id) }}">{{ $user->name }}</a>, ({{ $user->email }}) - 
-                <a href="{{ route('users.show', ['user' => $user]) }}">{{ $user->name }}</a>, ({{ $user->email }})</li>
+    <?php /*
+     * <li><a href="{{ action([UserController::class, 'show'], ['id' => $user->id]) }}">{{ $user->name }}</a>, ({{ $user->email }})</li>
+      Este no he conseguido que funcione
+     *                          */
+    ?>
 
-            @endforeach
-        </ul>
+    <li><a href="{{ url('/usuarios/'.$user->id) }}">{{ $user->name }}</a>, ({{ $user->email }}) - 
+        <a href="{{ route('users.show', ['user' => $user]) }}">{{ $user->name }}</a>, ({{ $user->email }})</li>
 
-        @endif
-    </p>
+    @endforeach
+</ul>
+
+@endif
+</p>
 @endsection
 
 
